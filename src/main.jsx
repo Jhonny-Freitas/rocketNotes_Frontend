4d89a16aafbebcaa92b4a42f1f48a536111bc65a
Jhonny Freitas <jhonnyfreitas348@gmail.com>
2023-06-01 00:07:55 -0300

    Created contexts and states

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import GlobalStyles from "./styles/global";
 
 import theme from "./styles/theme";
 
+import { AuthProvider } from "./hooks/auth";
+
 import { Routes } from "./routes";
 
 
@@ -12,7 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme = {theme}>
       <GlobalStyles />
-      <Routes />
+       <AuthProvider>
+          <Routes />
+        </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
